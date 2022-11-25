f565640eb49b304f08fa76c05bed2a76a5007300
khabibullakhanov <habibullaxonovmuhammadxon@gmail.com>
2022-11-26 02:20:29 +0500

    fix header

--- src/Companents/Navbar/NavigationTop.js
+++ src/Companents/Navbar/NavigationTop.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import "./NavigationTop.css"
 import myImage from "../../Assets/Images/My Image.jpg"
-import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import { useSelector, useDispatch } from 'react-redux';
+import {  useDispatch } from 'react-redux';
 import { acOpenMenu } from '../../Redux/OpenMenu';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { IconButton } from '@mui/material';
 
 export function NavigationTop() {
     const dispatch = useDispatch()
-    const menu = useSelector((state) => state.openMenu)
 
     return (
         <div id='navigation-top-main'>
