7080eb53e6e1c116ee65f13a9c821bbbd4188986
khabibullakhanov <habibullaxonovmuhammadxon@gmail.com>
2022-12-02 00:55:13 +0500

    fix navigation top

--- src/Companents/Navbar/NavigationTop.js
+++ src/Companents/Navbar/NavigationTop.js
@@ -1,32 +1,40 @@
-import React, { useState } from 'react'
+import React from 'react'
 import "./NavigationTop.css"
 import myImage from "../../Assets/Images/My Image.jpg"
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { IconButton } from '@mui/material';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import LogoutIcon from '@mui/icons-material/Logout';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
 
 export function NavigationTop() {
-    const [open, setOpen] = useState(false)
+
+    const menuOpen = (e) => {
+        window.location.reload();
+        localStorage.clear()
+        sessionStorage.clear()
+    }
+
     return (
         <div id='navigation-top-main'>
             <div id='navigation-more-info'>
-                <IconButton onClick={() => {
-                    setOpen(true)
-                }}>
-                    <MoreVertIcon />
-                </IconButton>
-                <Menu
-                    id='more-info-menu'
-                    open={open}
-                    onClose={() => {
-                        setOpen(!open);
-                    }}
-                    style={open ? { display: "block" } : { display: "none" }}>
-                    <MenuItem>Profile</MenuItem>
-                    <MenuItem>My account</MenuItem>
-                    <MenuItem>Logout</MenuItem>
-                </Menu>
+                <PopupState variant="popover" popupId="demo-popup-menu">
+                    {(popupState) => (
+
+                        <React.Fragment>
+                            <IconButton variant="contained" {...bindTrigger(popupState)}>
+                                <MoreVertIcon />
+                            </IconButton>
+                            <Menu {...bindMenu(popupState)}>
+                                <MenuItem onClick={() => { menuOpen(popupState.close) }} >Log Out <IconButton>
+                                    <LogoutIcon />
+                                </IconButton></MenuItem>
+                            </Menu>
+                        </React.Fragment>
+                    )}
+                </PopupState>
+
             </div>
             <figure >
                 <img src={myImage} alt="" />
